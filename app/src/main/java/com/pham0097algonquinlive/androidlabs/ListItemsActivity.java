027eb91dafc06ed9040fd64cf153e066121cfbe0
Duy&Phuong <pham0097@algonquincollege.com>
2017-01-25 08:31:59 -0500

    Added 3 activies for lab 2

---
+++ app/src/main/java/com/pham0097algonquinlive/androidlabs/ListItemsActivity.java
@@ -0,0 +1,13 @@
+package com.pham0097algonquinlive.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
